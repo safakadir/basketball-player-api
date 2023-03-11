21c752746e799e47f556bb85c1967a4e59db4871
safakadir <safakadir@gmail.com>
2023-03-11 07:58:55 +0300

    first commit: project bootstrap with spring initializr

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basketball Player Api
+
+A sample task to develop a Spring Boot application with GraphQL as a basketball player registration application.
---
+++ src/main/java/com/safakadir/basketballplayerapi/BasketballPlayerApiApplication.java
@@ -0,0 +1,13 @@
+package com.safakadir.basketballplayerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballPlayerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballPlayerApiApplication.class, args);
+	}
+
+}

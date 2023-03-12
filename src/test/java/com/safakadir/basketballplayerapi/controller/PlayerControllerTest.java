b9e308de7a1caf52065eedecb13d2d66f20ef22c
safakadir <safakadir@gmail.com>
2023-03-12 21:00:51 +0300

    test cases added for exception resolvers

--- src/test/java/com/safakadir/basketballplayerapi/controller/PlayerControllerTest.java
+++ src/test/java/com/safakadir/basketballplayerapi/controller/PlayerControllerTest.java
@@ -1,28 +1,37 @@
 package com.safakadir.basketballplayerapi.controller;
 
 import com.safakadir.basketballplayerapi.config.RsaJwtKeyProperties;
+import com.safakadir.basketballplayerapi.controller.exceptionresolver.ConstraintViolationExceptionResolver;
+import com.safakadir.basketballplayerapi.controller.exceptionresolver.MaximumCapacityReachedExceptionResolver;
+import com.safakadir.basketballplayerapi.exception.MaximumCapacityReachedException;
 import com.safakadir.basketballplayerapi.model.PlayerPosition;
 import com.safakadir.basketballplayerapi.service.PlayerService;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @GraphQlTest(PlayerController.class)
 class PlayerControllerTest {
 
     @MockBean
-    RsaJwtKeyProperties mockProperties;
+    RsaJwtKeyProperties mockProperties; //To prevent errors during initialization
 
     @Autowired
     GraphQlTester graphQlTester;
 
     @MockBean
     PlayerService playerService;
+
+    @Value("${app.maxTeamSize}")
+    int maxTeamSize;
 
     @Test
     void shouldQueryAllPlayers() {
@@ -47,7 +56,7 @@ class PlayerControllerTest {
     }
 
     @Test
-    void shouldRequestAddPlayerMutation() throws Exception {
+    void shouldRequestAddPlayerMutation() {
         // language=GraphQL
         String document = """
                 mutation {
@@ -66,7 +75,7 @@ class PlayerControllerTest {
     }
 
     @Test
-    void shouldRequestDeletePlayerMutation() throws Exception {
+    void shouldRequestDeletePlayerMutation() {
         // language=GraphQL
         String document = """
                 mutation {
@@ -77,5 +86,45 @@ class PlayerControllerTest {
                 .execute();
 
         verify(playerService, times(1)).deletePlayer(1);
+    }
+
+    @Test
+    void shouldThrowWhenArgumentsInvalid() {
+        String document = """
+            mutation {
+                addPlayer(name: "123", surname: "abc", position: C) {
+                    id
+                    name
+                    surname
+                }
+            }
+            """;
+
+        GraphQlTester.Response response = graphQlTester.document(document)
+                .execute();
+
+        response.errors().expect(e -> e.getErrorType().toString().equals(
+                ConstraintViolationExceptionResolver.ErrorType.ArgumentValidationError.name())
+        );
+    }
+
+    @Test
+    void shouldThrowWhenMaximumCapacity() {
+        String document = """
+            mutation {
+                addPlayer(name: "Name", surname: "Surname", position: C) {
+                    id
+                }
+            }
+            """;
+
+        when(playerService.addPlayer(any(), any(), any())).thenThrow(new MaximumCapacityReachedException("test"));
+
+        GraphQlTester.Response response = graphQlTester.document(document)
+                .execute();
+
+        response.errors().expect(e -> e.getErrorType().toString().equals(
+                MaximumCapacityReachedExceptionResolver.ErrorType.CapacityError.name())
+        );
     }
 }
